8eab20512c27ed487a509863c11be4ef12309031
B.Borza <B.Borza@mb.de>
2022-10-28 09:36:07 +0200

    Made form more clear

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,9 @@ namespace FingerSearchTree
             this.addBtn_ = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.deleteBtn_ = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // searchBtn_
@@ -43,7 +46,7 @@ namespace FingerSearchTree
             this.searchBtn_.TabIndex = 1;
             this.searchBtn_.Text = "Search";
             this.searchBtn_.UseVisualStyleBackColor = true;
-            this.searchBtn_.Click += new System.EventHandler(this.searchBtn__Click);
+            this.searchBtn_.Click += new System.EventHandler(this.SearchBtn__Click);
             // 
             // addBtn_
             // 
@@ -53,11 +56,11 @@ namespace FingerSearchTree
             this.addBtn_.TabIndex = 2;
             this.addBtn_.Text = "Add";
             this.addBtn_.UseVisualStyleBackColor = true;
-            this.addBtn_.Click += new System.EventHandler(this.addBtn__Click);
+            this.addBtn_.Click += new System.EventHandler(this.AddBtn__Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(197, 203);
+            this.textBox1.Location = new System.Drawing.Point(102, 101);
             this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(251, 20);
@@ -71,13 +74,46 @@ namespace FingerSearchTree
             this.deleteBtn_.TabIndex = 4;
             this.deleteBtn_.Text = "Delete";
             this.deleteBtn_.UseVisualStyleBackColor = true;
-            this.deleteBtn_.Click += new System.EventHandler(this.deleteBtn__Click);
+            this.deleteBtn_.Click += new System.EventHandler(this.DeleteBtn__Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.RandomInsert__Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.RamdomDelete__Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(373, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Random";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.RamdomUpdate__Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(480, 245);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.deleteBtn_);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.addBtn_);
@@ -94,6 +130,9 @@ namespace FingerSearchTree
         private System.Windows.Forms.Button addBtn_;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button deleteBtn_;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
